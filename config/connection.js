e6bb2c9e9fab3de5b10d574f0427e5edfbb53bd7
Darlene Holland <darleneholland3@gmail.com>
2018-05-13 14:06:03 -0400

    added database JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,3 @@
-// // Set up mysql connection
-// var mysql = require("mysql");
-// var connection;
-
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         port: 3306,
-//         host: "localhost",
-//         user: "root",
-//         password: "",
-//         database: "burgers_db"
-//     });
-// }
-
-// // Make connections
-// connection.connect(function (err) {
-//     if (err) {
-//         console.log("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection for our ORM to use
-// module.exports = connection;
 
 // *********************************************************************************
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
@@ -34,8 +7,8 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("burgers_db", "root", "", {
-  host: "localhost",
+var sequelize = new Sequelize("ow6ekwwl75gs2npt", "qh182g949bx63wcl", "rfn182v4n75hqyog", {
+  host: "s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
   pool: {
     max: 5,
